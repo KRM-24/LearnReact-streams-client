e4428f241f6357059fee266c42b3dc605be675a7
KRM-24 <36944142+KRM-24@users.noreply.github.com>
2020-06-08 15:17:22 -0500

    Rendering Authentication Status
    add .then method to set a 'isSignedIn' state from the auth instance
    display sign in status in Header

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,22 +1,38 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+  
   componentDidMount() {
     // window.gapi able to be called because we includes Google's JS library in index.html
     // passing callback function into load() so gapi.client.init gets called after the load function finishes
     window.gapi.load('client:auth2', () => {
+      // init returns a Promise so use .then after it finishes
       window.gapi.client.init( {
         // this clientId comes from console.developers.google.com - 'Streamy' project 
         clientId: '327906925820-5es1kd02njlnto4qlc0l9sm6pvdjupvr.apps.googleusercontent.com',
         scope: 'email'
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
       });
     });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
   }
   
   render() {
     return (
       <div>
-        Google Auth
+        {this.renderAuthButton()}
       </div>
     );
   }
