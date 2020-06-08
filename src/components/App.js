e3540a41588a82ce43f3d099fb9602269f24a9a0
KRM-24 <36944142+KRM-24@users.noreply.github.com>
2020-06-08 09:56:15 -0500

    navigation using BrowerserRouter
    use Link component instead of <a> tag to 'show/hide' PageOne or PageTwo instead of making a network request via <a> tag

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pageTwo">Navigate to Page Two</Link>
+    </div>
+  );
+}
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>Click Me!</button>
+      <Link to="/">Navigate to Page One</Link>
+    </div>
+  );
+}
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pageTwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
