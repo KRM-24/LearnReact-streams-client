a312ecb65c83f71afc0e62beb7d5d2a0e6ae3bca
KRM-24 <36944142+KRM-24@users.noreply.github.com>
2020-06-10 14:08:48 -0500

    Fetching a Stream for Edit Stream
    import fetchStream action and apply to connect method
    change from function to class based component to use the componentDidMount so the fetchStream action can be called when component/page loads

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = (props) => {
-  console.log(props);
-  return (
-    <div>
-      StreamEdit
-    </div>
-  );
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream){
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        {this.props.stream.title}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -16,4 +25,4 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
