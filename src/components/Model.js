52a614634b3d70fef331f6894300f2d67becd5a6
KRM-24 <36944142+KRM-24@users.noreply.github.com>
2020-06-13 13:49:28 -0500

    Hiding a Modal
    add styling and content to Modal

--- src/components/Model.js
+++ src/components/Model.js
@@ -5,7 +5,16 @@ const Modal = (props) => {
   return ReactDOM.createPortal(
     <div className="ui dimmer modals visible active">
       <div className="ui standard modal visible active">
-        some text here in a modal
+        <div className="header">
+          Delete Stream
+        </div>
+        <div className="content">
+          Are you sure you want to delete this stream?
+        </div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
       </div>
     </div>,
     document.querySelector('#modal')
